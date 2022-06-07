88b6fe071a7faab30b7ce191c2a83b36fae99088
FlutLab.io <>
2022-06-07 07:50:53 +0000

    api5

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sportsapp/FixtureScreen.dart';
 import 'package:sportsapp/MainScreen.dart';
-import 'package:sportsapp/ProfileScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,7 +12,6 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Widget> _widgetOptions = <Widget>[
     MainScreen(),
     FixtureScreen(),
-    ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -26,24 +24,20 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) => Scaffold(
         body: _widgetOptions.elementAt(_selectedIndex),
         bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.grey[800],
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today),
-                label: 'Fixtures',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.amber,
-            onTap: _onItemTapped,
-          ),
+          backgroundColor: Colors.grey[800],
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Fixtures',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber,
+          onTap: _onItemTapped,
+        ),
       );
 }
